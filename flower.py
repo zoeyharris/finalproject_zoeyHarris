7e2bfa2f0610ddded8ff436a966d8141833ae90b
Zoey Harris <zlh220000@utdallas.edu>
2026-05-11 11:20:28 -0500

    INITIATING screen settings

---
+++ flower.py
@@ -0,0 +1,11 @@
+import pygame
+import random
+import math
+
+#screen settings
+WIDTH = 800
+HEIGHT = 600
+SKY_BLUE = (200, 230, 255)
+GRASS_GREEN = (170, 220, 140)
+STEM_GREEN = (40, 140, 70)
+CENTER_BROWN = (120, 80, 40)
